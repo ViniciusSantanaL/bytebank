ff6685e2fcc636d0bca1bd105a6e8b3f3715872a
Vinicius Santana <viniciussantanaleao@gmail.com>
2022-03-27 20:34:27 -0300

    Melhorando uso do modulo de Rotas, recarregar componentes separados

--- src/app/nova-trasnferencia/nova-transferencia.component.ts
+++ src/app/nova-trasnferencia/nova-transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Transferencia } from './../../../models/transferencia.model';
 import { Component, EventEmitter, Output} from '@angular/core';
 import { TransferenciaService } from '../services/transferencia.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService){
+  constructor(private service: TransferenciaService, private router: Router){
 
   }
   transferir() {
@@ -25,6 +26,7 @@ export class NovaTransferenciaComponent {
       .subscribe((resultado) =>{
       console.log(resultado);
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     },
       (error) => console.error(error)
     );
